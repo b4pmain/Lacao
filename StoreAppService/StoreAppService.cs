baa33692f5254cf8df40c909cae90123c1e2c683
Naige30 <ishigamisenku0603@gmail.com>
2026-03-10 00:59:29 +0800

    1
    1

--- StoreAppService/StoreAppService.cs
+++ StoreAppService/StoreAppService.cs
@@ -6,17 +6,17 @@ using CRUDStoreDataService;
 
 namespace StoreAppService
 {
-    public class  StoreService
+    public class StoreService
     {
         private StoreRepository repo = new StoreRepository();
 
         public void PopulateDefaultStores()
         {
             repo.AddStore(new Store { Name = "Ligaya Store", Location = "Makati City", Profit = 100000, Expenses = 50000, Employees = 150, Products = 500 });
-            repo.AddStore(new Store { Name="Amaranth Store", Location="Muntinlupa City", Profit=999999, Expenses=42490, Employees=89, Products=300 });
-            repo.AddStore(new Store {Name="Rizen Store", Location="San Pedro Laguna", Profit=670000, Expenses=300509, Employees=67, Products=900 });
-        
-        
+            repo.AddStore(new Store { Name = "Amaranth Store", Location = "Muntinlupa City", Profit = 999999, Expenses = 42490, Employees = 89, Products = 300 });
+            repo.AddStore(new Store { Name = "Rizen Store", Location = "San Pedro Laguna", Profit = 670000, Expenses = 300509, Employees = 67, Products = 900 });
+
+
         }
         public void AddStore(Store store)
         {
@@ -46,6 +46,6 @@ namespace StoreAppService
             return false;
         }
     }
-    
-    
+
+
 }
--- StoreModels/StoreModels.cs
+++ StoreModels/StoreModels.cs
@@ -9,7 +9,7 @@ namespace StoreModels
         public string Name { get; set; }
         public string Location { get; set; }
         public double Profit { get; set; }
-        public double Expenses{ get; set; }
+        public double Expenses { get; set; }
         public int Employees { get; set; }
         public int Products { get; set; }
     }
